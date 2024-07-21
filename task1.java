b34bb460b4a592d14a5c23f23edd8fa6d6054487
Sahil Nimbalkar <149792419+sahilnim@users.noreply.github.com>
2024-07-21 15:33:27 +0530

    Add files via upload
    Assigned First Task To Create An Number Game

---
+++ task1.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class task1 {
+
+    public static int getrandN(int min, int max)
+    {
+        return(int)(Math.random()*(max-min+1)+min);
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int attempt = 3;//total number of attempts
+        int f_score = 0;//to store the score
+        boolean p_again = true;
+        System.out.println("Welcome To Number Game");
+        System.out.println("You have "+attempt+" chances to win");
+        while (p_again) {
+            int rand = getrandN(1,100);
+            boolean guess = false;
+            for (int i = 0; i < attempt ; i++) {
+                System.out.println("Attempt"+(i+1)+"  Guess the number");        
+                int user = sc.nextInt();
+                if (user == rand) {
+                    guess = true;
+                    f_score = +1;
+                    System.out.println("Congratulations match found successfully");
+                    break;
+                }
+                else if (user>rand) {
+                    System.out.println("Too High");
+                }
+                else{
+                System.out.println("Too Low");
+            }
+        }
+        if (guess==false) {
+            System.out.println("Better luck next time the correct number is "+rand);
+        }
+        System.out.println("Wish to play again(y/n)");
+        String playAgain = sc.next();
+        p_again=playAgain.equalsIgnoreCase("y");
+    }
+    System.out.println("Your Score is "+f_score);
+    System.out.println("Hope your done now");
+    System.out.println("Thankyou for playing");
+    System.out.println("Please Come Again !");
+    }
+}
