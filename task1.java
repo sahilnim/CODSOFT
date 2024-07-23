24f604e8b2552018444234a121cb7b05831e2c49
Sahil Nimbalkar <149792419+sahilnim@users.noreply.github.com>
2024-07-23 20:58:34 +0530

    Update task1.java

--- task1.java
+++ task1.java
@@ -2,23 +2,23 @@ import java.util.Scanner;
 
 public class task1 {
 
-    public static int getrandN(int min, int max)
+    public static int getrandomN(int min, int max)//method used to return value
     {
         return(int)(Math.random()*(max-min+1)+min);
     }
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        int attempt = 3;//total number of attempts
+        int attempt = 7;//total number of attempts
         int f_score = 0;//to store the score
         boolean p_again = true;
-        System.out.println("Welcome To Number Game");
+        System.out.println("- Welcome To Number Game -");
         System.out.println("You have "+attempt+" chances to win");
         while (p_again) {
-            int rand = getrandN(1,100);
+            int rand = getrandomN(1,100);
             boolean guess = false;
             for (int i = 0; i < attempt ; i++) {
-                System.out.println("Attempt"+(i+1)+"  Guess the number");        
+                System.out.println("Attempt "+(i+1)+"  Guess the number");        
                 int user = sc.nextInt();
                 if (user == rand) {
                     guess = true;
